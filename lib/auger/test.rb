2a0e6f27968eb85c6c6d052e0cca9d5e5740c8de
Ric Lister <rlister@gmail.com>
2012-05-25 00:05:40 -0400

    allow test to take no block: just returns response

--- lib/auger/test.rb
+++ lib/auger/test.rb
@@ -6,6 +6,12 @@ module Auger
       @name = name
       @block = block
     end
+    
+    ## you can call test with no block if you just want to return response value
+    def run(response)
+      @block ? @block.call(response) : response
+    end
+
   end
 
 end
