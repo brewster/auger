b5068c087a5bfa5e80a0e8e1d7b07a7241d48042
Ric Lister <rlister@gmail.com>
2012-06-01 14:03:07 -0400

    add server and role commands

--- lib/auger/project.rb
+++ lib/auger/project.rb
@@ -3,7 +3,7 @@ require 'host_range'
 module Auger
 
   class Project
-    attr_accessor :name, :fqdns, :hosts, :connections
+    attr_accessor :name, :fqdns, :hosts, :connections, :roles
     
     def self.load(name, &block)
       project = new(name)
@@ -16,13 +16,42 @@ module Auger
       @hosts = []
       @fqdns = []
       @connections = []
+      @roles = Hash.new { |h,k| h[k] = [] }
       self
     end
 
-    ## add host or return list of hosts
-    def hosts(*ranges)
-      ranges.empty? ? @hosts.flatten : @hosts << ranges.map {|r| HostRange.parse(r)}
+    def role(name, *args)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      servers = args.map { |arg| HostRange.parse(arg) }.flatten
+      servers.each { |server| roles[name] << server }
     end
+
+    def server(*args)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      roles = []
+      servers = []
+      args.each do |arg|
+        case arg
+        when Symbol then roles << arg
+        when String then servers << arg
+        else raise ArgumentError, "illegal argument to server: #{arg}"
+        end
+      end
+      roles = [nil] if roles.empty? # default role
+      roles.each { |name| role(name, *servers, options) }
+    end
+
+    alias :hosts :server
+
+    def servers(roles = [])
+      (roles.empty? ? @roles.values : @roles.values_at(*roles))
+        .flatten
+    end
+
+    ## add host or return list of hosts
+    # def hosts(*ranges)
+    #   ranges.empty? ? @hosts.flatten : @hosts << ranges.map {|r| HostRange.parse(r)}
+    # end
 
     alias :host :hosts
 
