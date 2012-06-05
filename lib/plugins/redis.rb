66e7e9918f5ad06e2d34fd7a1efa7e261aed0c9f
Ric Lister <rlister@gmail.com>
2012-06-05 16:24:08 -0400

    cleanup redis plugin

--- lib/plugins/redis.rb
+++ lib/plugins/redis.rb
@@ -1,3 +1,4 @@
+#require 'redis/connection'
 require 'redis'
 
 module Auger
@@ -10,7 +11,7 @@ module Auger
 
   class Redis < Auger::Connection
     def open(host, options)
-      ::Redis.new(options.merge(:host => host))
+      ::Redis.new options.merge({:host => host, :thread_safe => true})
     end
 
     def close(redis)
