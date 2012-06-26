9d2b189347a0ab473f3cb6f6f2e6cc7ed7ca9f51
Ric Lister <rlister@gmail.com>
2012-06-26 14:43:53 -0400

    rm cassandra plugins for now due to borked deps on ubuntu

--- lib/auger/result.rb
+++ lib/auger/result.rb
@@ -3,24 +3,6 @@ module Auger
   class Result
     attr_accessor :test, :outcome, :status
 
-    # def initialize(test, outcome)
-    #   @test = test
-    #   @outcome = outcome
-    # end
-
-    # def initialize(*args)
-    #   hash = args.last.is_a?(Hash) ? args.pop : {}
-    #   hash.each { |k,v| puts "got hash: #{k} = #{v}" }
-    #   (@outcome, @status) = args
-    # end
-
-    ## optional args are outcome, status, Hash of instance variables
-    # def initialize(*args)
-    #   hash = args.last.is_a?(Hash) ? args.pop : {}
-    #   (@outcome, @status) = args
-    #   hash.each { |k,v| self.instance_variable_set("@#{k}", v) }
-    # end
-
     def initialize(outcome = nil, status = outcome)
       @outcome = outcome
       @status  = status
