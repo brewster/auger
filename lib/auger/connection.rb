3bc10fad8ac7bbdd9bf7a753fc3b2da0a812858a
Ric Lister <rlister@gmail.com>
2012-10-26 16:47:55 -0400

    add ssh gateway and tunnel option

--- lib/auger/connection.rb
+++ lib/auger/connection.rb
@@ -1,7 +1,8 @@
+require 'net/ssh/gateway'
 module Auger
 
   class Connection
-    attr_accessor :requests, :connection, :response, :roles, :options
+    attr_accessor :requests, :connection, :response, :roles, :options, :gateway
 
     def self.load(port, &block)
       connection = new(port)
@@ -29,9 +30,14 @@ module Auger
       @options[method] = arg
     end
 
-    ## call plugin open() and return plugin-specific connection object, or exception
+    ## setup options and call appropriate connection open()
     def try_open(server)
-      options = @options.merge(server.options)
+      options = @options.merge(server.options) # merge connection and server options
+      options[:tunnel] ? try_open_tunnel(server, options) : try_open_direct(server, options)
+    end
+
+    ## call plugin open() and return plugin-specific connection object, or exception
+    def try_open_direct(server, options)
       begin
         self.open(server.name, options)
       rescue => e
@@ -39,10 +45,25 @@ module Auger
       end
     end
     
+    ## call plugin open() via an ssh tunnel
+    def try_open_tunnel(server, options)
+      host, user = options[:tunnel].split('@').reverse #for ssh to the gateway host
+      user ||= ENV['USER']
+      begin
+        @gateway = Net::SSH::Gateway.new(host, user)
+        gateway.open(server.name, options[:port]) do |port|
+          self.open('127.0.0.1', options.merge({:port => port}))
+        end
+      rescue => e
+        e
+      end
+    end
+
     ## safe way to call plugin close() (rescue if the connection did not exist)
     def try_close(conn)
       begin
         self.close(conn)
+        @gateway.shutdown! if @gateway
       rescue => e
         e
       end
