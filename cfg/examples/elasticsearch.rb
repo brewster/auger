cdca8805c8034cde0bafb242f5444c54ce119c06
Grant Heffernan <heffergm@gmail.com>
2012-11-28 14:36:57 -0500

    fix ES test example

--- cfg/examples/elasticsearch.rb
+++ cfg/examples/elasticsearch.rb
@@ -1,35 +1,97 @@
 require 'json'
 
-project "Elasticsearch" do
+project "Elasticsearch - Production" do
   server "localhost"
- 
+  
   http 9200 do
-    get "/_cluster/health" do
+    timeout 3
 
-      # this runs after request returns, but before tests
-      # use it to munge response body from json string into a hash
+    get "/" do
       before_tests do |r|
-        if r['Content-Type'].respond_to?(:match) and r['Content-Type'].match /application\/json/
-          begin 
-            r.body = JSON.parse(r.body)
-          rescue JSON::ParserError
-            puts "error parsing JSON in response body"
-          end
+        r.body = JSON.parse(r.body)
+        r
+      end
+
+      test "version" do |r|
+        status = r.body["version"]["number"]
+      end
+
+      test "snapshot build?" do |r|
+        build = r.body["version"]["snapshot_build"]
+        Result build.to_s, build == false
+      end
+
+    end
+
+    get "/_settings" do
+      before_tests do |r|
+        r.body = JSON.parse(r.body)
+        r
+      end
+
+      test "translog flush disabled?" do |r|
+        color = r.body["brew_production-0"]["settings"]["index.translog.disable_flush"]
+        status = case color
+          when 'false' then :ok
+          when 'true'  then :warn
+          else              :ok
         end
+        Result color, Status(status)
+      end
+    end
+
+    get "/_cluster/settings" do
+      before_tests do |r|
+        r.body = JSON.parse(r.body)
+        r
       end
 
-      # simple as it gets... did we get 200 back?
-      test "Status 200" do |r|
-        r.code == '200'
+      test "replica allocation disabled?" do |r|
+        color = r.body["transient"]["cluster.routing.allocation.disable_replica_allocation"]
+        status = case color
+          when 'false' then :ok
+          when 'true'  then :warn
+          else              :ok
+        end
+        Result color, Status(status)
+      end
+      test "shard allocation disabled?" do |r|
+        color = r.body["transient"]["cluster.routing.allocation.disable_allocation"]
+        status = case color
+          when 'false' then :ok
+          when 'true'  then :warn
+          else              :ok
+        end
+        Result color, Status(status)
       end
 
-      # an array of stats we want to collect
+    end
+
+    get "/_cluster/health" do
+      ## this runs after request returns, but before tests
+      ## use it to munge response body from json string into a hash
+      before_tests do |r|
+        r.body = JSON.parse(r.body)
+        r
+      end
+
+      test "HTTP Status" do |r|
+        Result r.code, r.code == '200'
+      end
+      
+      test "cluster status" do |r|
+        color = r.body["status"]
+        status = case color
+          when 'green'  then :ok
+          when 'yellow' then :warn
+          else               :error
+        end
+        Result color, Status(status)
+      end
+
       stats = %w[
         cluster_name
-        status
-        timed_out
         number_of_nodes
-        number_of_data_nodes
         active_primary_shards
         active_shards
         relocating_shards
@@ -37,39 +99,35 @@ project "Elasticsearch" do
         unassigned_shards
       ]
 
-      # loop through each stat
-      # if the body is a hash, return the value
       stats.each do |stat|
         test "#{stat}" do |r|
-          if r.body.is_a? Hash
-            r.body[stat]
-          else
-            false
-          end
+          r.body[stat]
         end
       end
 
-      # I've discovered that a typical fail case with elasticsearch is 
-      #   that on occassion, nodes will come up and not join the cluster
-      # This is an easy way to see if the number of nodes that the host 
-      #   actually sees (actual_data_nodes) matches what we're
-      #   expecting (expected_data_nodes).
-      # TODO: dynamically update expected_data_nodes based on defined hosts:
-      test "Expected vs Actual Nodes" do |r|
-        if r.body.is_a? Hash
-          expected_data_nodes = 8
-          actual_data_nodes = r.body['number_of_data_nodes']
-
-          if expected_data_nodes == actual_data_nodes
-            true
-          else
-            false
-          end
-        else
-          false
-        end
+      test "number of data nodes" do |r|
+        nodes = r.body['number_of_data_nodes']
+        Result nodes, nodes == 4
       end
+
     end
-  end
-end
 
+    get "/_nodes/_local/stats?all" do
+      before_tests do |r|
+        JSON.parse(r.body)
+      end
+
+      test "open file descriptors" do |r|
+        r['nodes'].first[1]['process']['open_file_descriptors']
+      end
+
+      test "heap used" do |r|
+        used, committed = r["nodes"].first[1]['jvm']['mem'].values_at('heap_used','heap_committed')
+        "#{used} (of #{committed})"
+      end
+
+    end
+
+  end
+
+end
